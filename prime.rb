5466521b6334d4c0d407111246471bd8f88d2b72
James Dodds <dodds1990@gmail.com>
2020-03-13 01:17:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  if num <= -1
+    return false
+  else
+    i = 2
+    while i < num do
+      return false if num % i == 0
+      i += 1
+    end
+    return true
+  end
+end
